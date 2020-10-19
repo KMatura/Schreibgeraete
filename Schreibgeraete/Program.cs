b69e34f311fd9d56bdd52f552994e15a8aa66674
KMatura <matura.k04@htlwienwest.at>
2020-10-19 19:49:51 +0200

    Projektdateien hinzufügen.

---
+++ Schreibgeraete/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schreibgeraete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<string> Sätze = new List<string>();
+            Sätze.Add("Es ist Herbst");
+            Sätze.Add("Es ist gerade Herbst");
+            Sätze.Add("Der Aktuelle Monat ist Herbst");
+            Sätze.Add("Der Sommer ist vorbei und der Winter naht");
+            List<Schreibgeraet> Stifte = new List<Schreibgeraet>();
+            Stifte.Add(new Buntstift(ConsoleColor.Green, 2.0));
+            Stifte.Add(new Buntstift(ConsoleColor.Red, 2.0));
+            Stifte.Add(new Kugelschreiber(ConsoleColor.DarkGray, 0.7));
+            Stifte.Add(new Kugelschreiber(ConsoleColor.Blue, 0.7));
+            foreach (var item in Stifte)
+            {
+                int Satz = rnd.Next(1, 4);
+                item.SchreibeText(Sätze[Satz], item.Farbe);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Schreibgeraete/Schreibgeraet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schreibgeraete
+{
+    abstract class Schreibgeraet
+    {
+        public double Strichstärke { get; set; }
+        public ConsoleColor Farbe { get; set; }
+        public string Typ { get; set; }
+        public Schreibgeraet(double Strichstärke)
+        {
+            this.Strichstärke = Strichstärke;
+            Farbe = ConsoleColor.Black;
+            Typ = "unbekannt";
+        }
+        public Schreibgeraet(double Strichstärke, ConsoleColor Farbe, string Typ)
+        {
+            this.Farbe = Farbe;
+            this.Strichstärke = Strichstärke;
+            this.Typ = Typ;
+        }
+        public void SchreibeText(string text, ConsoleColor Farbe)
+        {
+            Console.ForegroundColor = Farbe;
+            Console.WriteLine($"(Typ: {this.Typ}, Farbe:{this.Farbe}, Strichstärke:{this.Strichstärke})schreibt: {text}");
+        }
+    }
+}
